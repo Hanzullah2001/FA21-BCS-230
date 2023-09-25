c9e800ac2b0c7bac72c76536ccee17852b62f1f6
Hanzullah2001 <146021072+Hanzullah2001@users.noreply.github.com>
2023-09-25 11:33:30 -0700

    app.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'; // Corrected the placement of this import
+
+import Button from './components/button'; // Corrected the filename to 'Button'
+
+function App() {
+  return (
+    <div>
+      <Button />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Button() {
+  return (
+    <>
+    <button>
+      Register
+    </button>
+    
+    </>
+  );
+}
+
+export default Button;
